b1815fb63f5c71973779deecf4a90ca092baaf65
Nick-Brinegar <brinegar.nicholas@gmail.com>
2024-10-09 19:30:10 -0600

    start client and cleanup on exit

--- run.sh
+++ run.sh
@@ -7,3 +7,22 @@ start_server() {
     SPRING_PID=$!
     echo "Server started with PID: $SPRING_PID"
 }
+
+start_client() {
+    echo "Starting client..."
+    cd ./client
+    npm start &
+    CLIENT_PID=$!
+    echo "Client started with PID: $CLIENT_PID"
+}
+
+cleanup() {
+    echo "Stopping server..."
+    kill $SPRING_PID
+    echo "Server stopped."
+    echo "Stopping client..."
+    kill $CLIENT_PID
+    echo "Client stopped."
+    exit 0
+}
+
