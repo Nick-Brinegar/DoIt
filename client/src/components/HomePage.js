1585430337a0d4548a9443e3a94634d901001ae0
Nick-Brinegar <brinegar.nicholas@gmail.com>
2024-10-13 11:46:53 -0600

    Create Home Page component

---
+++ client/src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TaskManager from './TaskManager';
+
+const HomePage = () => {
+    return (
+        <div className="homepage-container">
+            <TaskManager />
+        </div>
+    );
+};
+
+export default HomePage;
