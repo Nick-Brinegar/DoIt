77fc9eae290a6b5f34e607ccb321524a42b59767
Nick-Brinegar <brinegar.nicholas@gmail.com>
2024-10-13 12:11:41 -0600

    Finish styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-import TaskList from './components/TaskList';
-import TaskForm from './components/TaskForm';
 import HomePage from './components/HomePage';
 
 function App() {
